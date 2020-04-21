00d9bc69961b5cb0d68e27e0493a0a1e27f12e99
Tarik <49348194+tarizer@users.noreply.github.com>
2020-04-21 12:45:36 +0000

    Add Display and button components to counter

--- src/react-getting-started/components/Counter.js
+++ src/react-getting-started/components/Counter.js
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
 
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
+const Button = ({ onClickFunction, increment }) => {
+  const handleClick = () => onClickFunction(increment);
+
+  return (
+    <button type="submit" onClick={handleClick}>
+      {increment}
+    </button>
+  );
+};
+
+function Display({ message }) {
+  document.querySelector("title").innerText = `Simple counter: ${message}`;
 
   return (
     <>
-      <div>Hello from Counter</div>
       <p>
-        <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+        <span aria-label="React emoji" role="img">
+          ⚛️
+        </span>
+        Hello from Counter: {message}
       </p>
     </>
   );
-};
+}
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) =>
+    setCounter(counter + incrementValue);
+
+  return (
+    <>
+      <Button onClickFunction={incrementCounter} increment={-10} />
+      <Button onClickFunction={incrementCounter} increment={-5} />
+      <Button onClickFunction={incrementCounter} increment={-1} />
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Display message={counter} />
+    </>
+  );
+}
 
 export default Counter;
