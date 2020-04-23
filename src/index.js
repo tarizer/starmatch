4a6319ff20cad1071768165dee7177fcbd713069
Tarik <49348194+tarizer@users.noreply.github.com>
2020-04-23 12:48:10 +0000

    Add Star Match game

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+
 import Counter from "./react-getting-started/components/Counter";
 import GithubCards from "./react-getting-started/components/GithubCards";
+import StarMatch from "./react-getting-started/components/StarMatch";
 
-let runApp = "GithubCards";
+let runApp = "StarMatch";
 
 function SelectApp({ appName }) {
   switch (appName) {
-    case "App":
-      console.log("Inside App");
-      return <App />;
     case "Counter":
       console.log("Inside Counter");
       return <Counter />;
     case "GithubCards":
       console.log("Inside GithubCards");
       return <GithubCards title="⚛️ The GitHub Cards App" />;
+    case "StarMatch":
+      console.log("Inside Star Match");
+      return <StarMatch title="⚛️ The Star Match Game" />;
 
     default:
       console.log("No app selected");
